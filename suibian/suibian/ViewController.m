f9f5ed63bc85715bd36df81133404c712a6842ea
zjtd <1032894207@qq.com>
2016-02-17 09:45:34 +0800

    第二次

--- suibian/suibian/ViewController.m
+++ suibian/suibian/ViewController.m
@@ -17,6 +17,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    UILabel *lab = [[UILabel alloc] init];
+    
+    [self.view addSubview:lab];
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
